c4004b4a504be83865bb942c3da5e0f61e0aa474
Valentina-Gallego-Ramos <144750192+Valentina-Gallego-Ramos@users.noreply.github.com>
2023-09-30 08:58:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const fruits = ["Bananas", "orange" , "apple" , " mango "]
-let text () => fruits.toString
-  console.table(fruits) ();
-
+const fruits = ["Bananas","orange","apple"," mango "]
+let text = fruits.toString()
+document.write(fruits);
+// 1 variable = un espacion en memoria
+text = "Bananas,orange,apple,mango";
   
